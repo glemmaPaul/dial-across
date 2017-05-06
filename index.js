df45963ad11b21a0fbfb19aa0a08c7e860b13432
Paul Oostenrijk <pauloostenrijk@Pauls-MacBook-Pro.local>
2017-05-06 11:19:53 -0400

    feat(public): Added public service
    feat(index): Added a line to serve the public folder

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ import config from 'config'
 var app = express()
 app = dialAcross.attach(app)
 
+app.use(express.static('public'))
+
 app.listen(config.get('Server.port'), function() {
     console.log("Successfully attached to " + config.get('Server.port'))
 })
